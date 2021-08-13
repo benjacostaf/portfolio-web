1092c6b119b0c910eb0c410e58626f94e66a9076
Benjamin Costa Franceschi <benjacostaf@gmail.com>
2021-08-13 13:34:27 -0300

    implement tymon/jwt composer

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -95,9 +95,10 @@ $app->middleware([
 |
 */
 
-// $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
-// $app->register(App\Providers\EventServiceProvider::class);
+ $app->register(App\Providers\AppServiceProvider::class);
+ $app->register(App\Providers\AuthServiceProvider::class);
+ $app->register(App\Providers\EventServiceProvider::class);
+ $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
